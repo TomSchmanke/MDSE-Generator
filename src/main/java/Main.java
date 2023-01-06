b3ce6177f9f7ae6fc770b048651ce9576352b706
Tom <tomschmanke@gmx.de>
2023-01-06 23:39:02 +0100

    Add controller generation code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,5 +11,6 @@ public class Main {
         initializeProject.unzipFile(nameOfZip, nameOfZip.substring(0, nameOfZip.length() - 3));
 
         TemplateResolver templateResolver = new TemplateResolver();
+        templateResolver.createControllerFiles();
     }
 }
--- src/main/java/TemplateResolver.java
+++ src/main/java/TemplateResolver.java
@@ -1,5 +1,6 @@
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import template_data.ControllerDataModel;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,39 +11,42 @@ import java.util.Properties;
 
 public class TemplateResolver {
 
-    static String inputTemplate = "methode.vm";
-    static String annotation = "GetMapping";
-    static String crudPath = "/hersteller";
-    static String crudParameter = "/{id}";
-    static String privatePublic = "public";
-    static String returnType = "Hersteller";
-    static String methodName = "get";
-    static String methodParameters = "@PathVariable int id";
-    static List<String> allStatements = Arrays.asList("Optional<Hersteller> ret = repository.findById(id)", "if (ret.isPresent()) return ret.get();", "return null;");
-    static String outputFile = "test.java";
+    private final VelocityEngine velocityEngine;
+    private final List<String> entityNames = Arrays.asList("Antrag", "Benutzer", "Fahrzeug", "Hersteller");
 
-    public TemplateResolver() throws IOException {
-        VelocityEngine velocityEngine = new VelocityEngine();
+    public TemplateResolver() {
+        velocityEngine = new VelocityEngine();
         Properties velocityProperties = new Properties();
         velocityProperties.put("file.resource.loader.path", "src/main/resources/");
         velocityEngine.init(velocityProperties);
-
-        VelocityContext context = new VelocityContext();
-        context.put("annotation", annotation);
-        context.put("crudPath", crudPath);
-        context.put("crudParameter", crudParameter);
-        context.put("privatePublic", privatePublic);
-        context.put("returnType", returnType);
-        context.put("methodName", methodName);
-        context.put("methodParameters", methodParameters);
-        context.put("allStatements", allStatements);
-
-        Writer writer = new FileWriter(outputFile);
-        velocityEngine.mergeTemplate(inputTemplate, "UTF-8", context, writer);
-        writer.flush();
-        writer.close();
-
-        System.out.println("Generated " + outputFile);
     }
+
+    private void resolveTemplate(VelocityContext velocityContext, String inputTemplate, String outputFile) {
+        try {
+            Writer writer = new FileWriter(outputFile);
+            velocityEngine.mergeTemplate(inputTemplate, "UTF-8", velocityContext, writer);
+            writer.flush();
+            writer.close();
+            System.out.println("Successfully generated " + outputFile);
+        } catch (IOException e) {
+            System.out.println("Error occurred during merging of template and velocity context " + e);
+        }
+    }
+
+    public void createControllerFiles() {
+        for (String entity: entityNames) {
+            VelocityContext velocityContext = new VelocityContext();
+            velocityContext.put("targetPackagePath" , "TODO");
+            velocityContext.put("entitiesPackagePath" , "TODO");
+            velocityContext.put("repositoriesPackagePath" , "TODO");
+
+            ControllerDataModel controllerDataModel = new ControllerDataModel(entity, "id");
+            velocityContext.put("controller", controllerDataModel);
+
+            resolveTemplate(velocityContext, "controller.vm", entity + "Controller.java");
+        }
+    }
+
+
 
 }
