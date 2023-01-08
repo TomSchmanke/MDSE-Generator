bf9f51df00543fac6c6e9c5428e58982d3aee6a5
Laura Schmidt <lauschmidt.ls@gmail.com>
2023-01-08 14:22:40 +0100

    add converter lib

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import de.arinir.mdsd.metamodell.MDSDMetamodell.UMLClassDiagramm;
+
 import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         InitializeProject initializeProject = new InitializeProject();
         String nameOfZip = initializeProject.loadGeneratedFilesFromSpringInitializer();
@@ -10,5 +13,8 @@ public class Main {
         }
         initializeProject.unzipFile(nameOfZip, nameOfZip.substring(0, nameOfZip.length() - 3));
 
+        XMLConverter xmlConverter = new XMLConverter();
+        UMLClassDiagramm diagramm = xmlConverter.processXMLUMLFile("/Flottenmanagement.xml");
+        System.out.println(diagramm);
     }
 }
---
+++ src/main/java/XMLConverter.java
@@ -0,0 +1,28 @@
+import de.arinir.XMI2MDSDMetamodellConverter.XMI2MDSDMetamodellConverter.Converter;
+import de.arinir.mdsd.metamodell.MDSDMetamodell.UMLClassDiagramm;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Helper class to convert XML file to MDSDMetamodell object.
+ *
+ * @author Laura Schmidt
+ */
+public class XMLConverter {
+
+    /**
+     * Used to process a XML file representing a UML class diagram. The file is read and
+     * transformed into the MDSDMetamodell.
+     *
+     * @param path Path to XML file. File needs to be stored in resources directory
+     * @return Parsed XML file as UMLClassDiagramm.
+     * @throws IOException Thrown when path is not valid.
+     */
+    public UMLClassDiagramm processXMLUMLFile(String path) throws IOException {
+        try (InputStream s = Main.class.getResourceAsStream(path)) {
+            Converter converter = new Converter();
+            return converter.convert(s);
+        }
+    }
+}
