7729524efdab2de73dfe7778fa40fc23dad395d0
Jonas Knebel <jonas.knebel@gmx.de>
2023-01-31 20:27:17 +0100

    added file reader and write for user code

--- src/main/java/util/CreateProjectStructureAsJson.java
+++ src/main/java/util/CreateProjectStructureAsJson.java
@@ -1,16 +1,18 @@
 package util;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class CreateProjectStructureAsJson {
@@ -25,12 +27,42 @@ public class CreateProjectStructureAsJson {
             }
             ObjectMapper mapper = new ObjectMapper();
             ObjectNode rootNode = mapper.createObjectNode();
+            BufferedReader reader;
             for(File file: filePaths) {
-                rootNode.set(file.toString(), null);
+                try {
+                    reader = new BufferedReader(new FileReader(file));
+                    ArrayList<String> fileStringList = new ArrayList<>();
+                    String line = reader.readLine();
+                    while (line != null) {
+
+                        line = reader.readLine();
+                        if(line.startsWith("//MDSD-GENERATED ")){
+                            fileStringList.add(line);
+                        }
+
+                    }
+                    reader.close();
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    String jsonString = objectMapper.writeValueAsString(fileStringList);
+                    JsonNode jsonNode = objectMapper.readValue(jsonString, JsonNode.class);
+                    rootNode.set(file.toString(), jsonNode);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
             }
+        writeJSONNodeToFile(mapper.writeValueAsString(rootNode));
         System.out.println(rootNode);
-        }
     }
+
+    private void writeJSONNodeToFile(String rootNodeAsString) throws IOException {
+        File file = new File("output.json");
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write(rootNodeAsString);
+        fileWriter.close();
+    }
+}
 
 
 
